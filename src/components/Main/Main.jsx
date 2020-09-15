663e1bbda8939b7a716b09cdcd5481f736358b45
Agucarpar <agucp@outlook.es>
2020-09-15 11:54:02 +0200

    create main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Main = () => {
+    return (
+        <div>
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+
+
+export default Main
