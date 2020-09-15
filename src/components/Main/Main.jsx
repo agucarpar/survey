a8e48bc7074356989af1f901f928a75202b4ac42
Agucarpar <agucp@outlook.es>
2020-09-15 16:07:59 +0200

    launching thequestions

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Questions from '../../utils/const/questions'
 import './main.scss'
 
 const Main = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [scored, setScored] = useState(0)
+    
+
+
+    const chekingIfCorrect = (isCorrect) => {
+        setCurrentQuestion(currentQuestion+1)
+
+        if(isCorrect){
+            setScored(scored+1)
+        }
+
+    }
+
     return (
         <div>
-            <p className='color'>Hello World!</p>
+            <p className='color'>{Questions[currentQuestion].questionText}</p>
+            <div>
+                {Questions[currentQuestion].answersOptions.map(answer=>(
+
+                    
+                    <button onClick={()=>chekingIfCorrect(answer.isCorrect)}>{answer.answerText}</button>
+                        
+                    )
+                
+                )}
+            </div>
         </div>
     )
 }
