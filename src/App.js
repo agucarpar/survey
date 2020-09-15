31bb4fcdf6185d2b2e8123cfa1f5c70a5ddba03b
Agucarpar <agucp@outlook.es>
2020-09-15 12:56:51 +0200

    removing app scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.scss';
 import Main from './components/Main/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Main/>
     </div>
   );
