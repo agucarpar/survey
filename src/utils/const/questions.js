4ccea0ebcde38533f07c83bb58f78c556556c7ed
Agucarpar <agucp@outlook.es>
2020-09-15 16:08:10 +0200

    creations questions

---
+++ src/utils/const/questions.js
@@ -0,0 +1,19 @@
+const questions =[
+    {
+        questionText: "Manolito Gafotasse fue al Parque",
+        answersOptions:[
+            {answerText : "No", isCorrect: true},
+            {answerText: "Sí", isCorrect: false}
+        ]
+    },
+    {
+        questionText: "Paco va al campo",
+        answersOptions:[
+            {answerText : "No", isCorrect: true},
+            {answerText: "Sí", isCorrect: false}
+        ]
+    },
+]
+
+
+export default questions
